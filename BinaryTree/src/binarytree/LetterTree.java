3e7b533621aacd26f8dcf484e1020e37f635f628
Kurt <Kurt@10.29.1.4>
2016-03-11 11:34:37 -0600

    answer code

--- BinaryTree/src/binarytree/LetterTree.java
+++ BinaryTree/src/binarytree/LetterTree.java
@@ -1,16 +1,38 @@
 package binarytree;
 
-/**
- *
- * @author add your name here
- */
-public class LetterTree {
+//********************************************************************
+//  LetterTree.java     
+//
+//  Creates a binary tree storing letters at each node, then
+//  prints the letters using a postorder traversal.
+//********************************************************************
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+
+public class LetterTree
+{
+   public static void main (String[] args)
+   {
+      TreeNode h = new TreeNode("h", null, null);
+      TreeNode e = new TreeNode("e", null, null);
+      TreeNode l1 = new TreeNode("l", h, e);
+      TreeNode l2 = new TreeNode("l", null, null);
+      TreeNode o = new TreeNode("o", l1, l2);
+
+      postorder(o);
+   }
+
+   //-----------------------------------------------------------------
+   //  Prints each item in the tree with the given root using
+   //  a postorder traversal.
+   //-----------------------------------------------------------------
+   private static void postorder (TreeNode current)
+   {
+      if (current.getLeft() != null)
+         postorder(current.getLeft());
+
+      if (current.getRight() != null)
+         postorder(current.getRight());
+
+      System.out.println(current.getValue());
+   }
 }
